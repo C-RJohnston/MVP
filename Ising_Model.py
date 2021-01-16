0126ffcbb11e2317e3a9eace080b02d3784bff60
Cameron <cameron.robin.johnston@gmail.com>
2021-01-16 21:27:41 +0000

    added animation

--- Ising_Model.py
+++ Ising_Model.py
@@ -2,7 +2,8 @@ import random
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib.colors import ListedColormap
+from matplotlib import animation
 
 
 #params
@@ -14,13 +15,15 @@ vals = np.array([RED,BLUE])
 cmap = ListedColormap(vals)
 
 class Lattice:
-    def __init__(self,WIDTH:int,HEIGHT:int,UP_COLOUR:list,DOWN_COLOUR:list):
+    def __init__(self,WIDTH:int,HEIGHT:int,T:float,UP_COLOUR:list,DOWN_COLOUR:list):
         """
         default constructor for the lattice, initialise with
 
         WIDTH: int width of the grid
 
         HEIGHT: height of the grid
+
+        T: Temperature of the system
 
         UP_COLOUR: colour with which the up spin is drawn
 
@@ -30,11 +33,15 @@ class Lattice:
         self._Y = HEIGHT        
         #generate a random spin lattice of -1 and 1
         #first generate a random int array of 0s and 1s of size WIDTH,HEIGHT
-        self._spins = np.random.randint(2,size=(self._Y,self._X))
+        self._spins = self.create_lattice()
         #replace 0 with -1
         self._spins = np.where(self._spins==0,-1,self._spins)
         #define the colour map for drawing the array
         self.cmap = ListedColormap(np.array([DOWN_COLOUR,UP_COLOUR]))
+        self.T = T
+
+    def create_lattice(self):
+        return np.random.randint(2,size=(self._Y,self._X))
 
     def __iter__(self):
         return iter(self._spins)
@@ -72,7 +79,8 @@ class Lattice:
         The total energy of the system is thus the sum of these energies. The difference between the two states after flipping one spin 
         is given by the change in energy of the neighbourhood of the flipped spin
         """
-        spin = self._spins[y][x]
+
+        spin = self._spins[y,x]
         neighbours = self.get_neighbours(x,y)
 
         #determine initial energy pre spin-flip
@@ -85,7 +93,6 @@ class Lattice:
         E_f = 0
         for neighbour in neighbours:
             E_f+=-spin*neighbour
-        self._spins[y][x]*=-1
         return E_f-E_i
 
     def metropolis(self):
@@ -93,6 +100,26 @@ class Lattice:
         #choose a random spin within the grid
         x = random.randrange(self._X)
         y = random.randrange(self._Y)
+        dE = self.calc_delta_energy(x,y)
+        #the probability that the spin should flip
+        p = min(1,np.exp(-dE/self.T))
+        if(random.random()<p):
+            #flip the spin based on the determined probability
+            self._spins[y,x]*=-1
+        return self._spins
+        
+    def systematic_metropolis(self):
+        """the same functionality as metropolis function, but for each spin in the state"""
+        for y in range(self._Y):
+            for x in range(self._X):
+                dE = self.calc_delta_energy(x,y)
+                #the probability that the spin should flip
+                p = min(1,np.exp(-dE/self.T))
+                if(random.random()<p):
+                    #flip the spin based on the determined probability
+                    self._spins[y,x]*=-1
+        return self._spins
+
 
     def calc_total_energy(self):
         """
@@ -114,7 +141,18 @@ class Lattice:
         fig,ax = plt.subplots()
         ax.pcolormesh(self._spins,cmap = self.cmap)
         plt.show()
+    def anim(self):
+        fig,ax = plt.subplots()
+        im = plt.imshow(self._spins,cmap=self.cmap)
+        def animate(i):
+            im.set_array(self.systematic_metropolis())
+            return im,
+
+        a = animation.FuncAnimation(fig,animate,interval=1)
+        plt.show()
+
     
 
 
-L = Lattice(50,50,[0,0,1,1],[1,0,0,1])
+L = Lattice(50,50,1,[0,0,1,1],[1,0,0,1])
+L.anim()
